bf94b0e3008ad3ed22d58bb047ac8fd9defcc93f
Bluemi <bruno.schilling@protonmail.ch>
2018-10-13 16:11:45 +0200

    Implemented Game Logic

--- funfairfight/Assets/GameManagerScript.cs
+++ funfairfight/Assets/GameManagerScript.cs
@@ -1,18 +1,134 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+enum Attack
+{
+    Unselected,
+    FerrisWheel,
+    AirGun,
+    HitTheLukas,
+    AutoScooter,
+    Grabbler,
+}
 
 public class GameManagerScript : MonoBehaviour
 {
+    public GameObject[] Players;
+    public Text ShowText;
+
+    private Attack[] player_choices;
+    private bool round_running;
+
+    // Shows the Countdown
+    IEnumerator StartRound()
+    {
+        ShowText.text = "5";
+        yield return new WaitForSeconds(1.0f);
+        ShowText.text = "4";
+        yield return new WaitForSeconds(1.0f);
+        ShowText.text = "Fun";
+        yield return new WaitForSeconds(0.5f);
+        ShowText.text = "Fair";
+        yield return new WaitForSeconds(0.5f);
+        ShowText.text = "Fight";
+        Fight();
+        yield return new WaitForSeconds(0.5f);
+        ShowText.text = "";
+        PlayAnimation();
+        yield return new WaitForSeconds(2.0f);
+        round_running = false;
+    }
+
+    void PlayAnimation()
+    {
+        // TODO
+    }
+
+    void PlayerAttackChoice(int player_id, Attack attack)
+    {
+        if (player_id >= 0 && player_id < 2)
+        {
+            player_choices[player_id] = attack;
+        } else {
+            Debug.Log("ERROR: player_id = " + player_id.ToString());
+        }
+    }
+
+    void Fight()
+    {
+        int winner = GetWinner(player_choices[0], player_choices[1]);
+        if (winner != -1) {
+            int looser = 1 - winner;
+            Players[looser].GetComponent<PlayerScript>().DecLivePoints();
+        }
+    }
+
+    // returns the Attacks, which loose against <attack>
+    Attack[] GetLosingAttacks(Attack attack)
+    {
+        switch (attack)
+        {
+            case Attack.Unselected:
+                return new Attack[0];
+            case Attack.AirGun:
+                return new[] {Attack.FerrisWheel, Attack.HitTheLukas};
+        }
+
+        return new Attack[0]; // TODO
+    }
+
+    // if the first attack wins: 0
+    // if second attack wins: 1
+    // if draw: -1
+    int GetWinner(Attack first_attack, Attack second_attack)
+    {
+        if (first_attack == second_attack)
+        {
+            return -1;
+        } else {
+            Attack[] loosing_attacks = GetLosingAttacks(first_attack);
+            foreach (Attack a in loosing_attacks)
+            {
+                // If the second player chosen a losing attack
+                if (second_attack == a)
+                {
+                    // First player wins
+                    return 0;
+                }
+            }
+            // Otherwise the second player wins
+            return 1;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello world!");
+        ResetPlayerChoices();
+        round_running = false;
+    }
+
+    void ResetPlayerChoices()
+    {
+        player_choices = new[] { Attack.Unselected, Attack.Unselected };
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!round_running)
+        {
+            round_running = true;
+            ResetPlayerChoices();
+
+            // TODO remove
+            PlayerAttackChoice(0, Attack.AirGun);
+            PlayerAttackChoice(1, Attack.AutoScooter);
+            
+            StartCoroutine("StartRound");
+        }
     }
 }
---
+++ funfairfight/Assets/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Button[] Buttons;
+    private int LivePoints;
+
+    public Text LiveText;
+
+    private const int NUM_BUTTONS = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LivePoints = 3;
+    }
+
+    public void DecLivePoints()
+    {
+        LivePoints--;
+        LiveText.text = LivePoints.ToString();
+    }
+
+    public int GetLivePoints()
+    {
+        return LivePoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
