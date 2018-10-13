e04b893da3c958856f92dd74f5e145cb50bc1af7
memoryleak47 <memoryleak47@protonmail.ch>
2018-10-13 22:36:53 +0200

    reordered HealthMark / AttackButton position

--- funfairfight/Assets/PlayerScript.cs
+++ funfairfight/Assets/PlayerScript.cs
@@ -21,7 +21,7 @@ public class PlayerScript : MonoBehaviour
         Attack[] attacks = new[] { Attack.AirGun, Attack.AutoScooter, Attack.FerrisWheel, Attack.Grabbler, Attack.HitTheLukas };
 		HealthMarks = new GameObject[MAX_HEALTH];
 		for (int i = 0; i < MAX_HEALTH; i++) {
-			HealthMarks[i] = Instantiate(GameManagerScript.Instance.HealthPrefab, new Vector3(PlayerID*4.0f-2f + i, 4.0f, 0.0f), Quaternion.identity);
+			HealthMarks[i] = Instantiate(GameManagerScript.Instance.HealthPrefab, GetHealthMarkPosition(i), Quaternion.identity);
 		}
 		Health = MAX_HEALTH;
 
@@ -29,7 +29,7 @@ public class PlayerScript : MonoBehaviour
 
         for (int i = 0; i < attacks.Length; i++)
         {
-            GameObject btn = Instantiate(ButtonPrefab, new Vector3(PlayerID*3.0f-1.5f, i*0.8f, 0.0f), Quaternion.identity);
+            GameObject btn = Instantiate(ButtonPrefab, GetAttackButtonPosition(i), Quaternion.identity);
             btn.GetComponent<AttackButtonScript>().PlayerID = PlayerID;
             btn.GetComponent<AttackButtonScript>().attack = attacks[i];
             Buttons[i] = btn.GetComponent<Sprite>();
@@ -47,6 +47,50 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+	int GetDirection() {
+		return 2*PlayerID - 1;
+	}
+
+	float Factor() {
+		return (Camera.main.WorldToViewportPoint(new Vector3(1, 0, 0)) - Camera.main.WorldToViewportPoint(new Vector3(0, 0, 0))).magnitude;
+	}
+
+	Vector3 GetHealthMarkPositionViewport(int i) {
+		float size = Factor() * GameManagerScript.Instance.HealthPrefab.transform.lossyScale.x;
+
+		float x = 0.5f - (size + i * 3.0f*size/2.0f);
+		float y = 1.0f - (3.0f * size / 2.0f);
+		if (PlayerID == 1) {
+			x = 1.0f - x;
+		}
+		return new Vector3(x, y, 0.0f);
+	}
+
+	Vector3 GetAttackButtonPositionViewport(int i) {
+		float size = Factor() * GameManagerScript.Instance.PressedAutoScooter.bounds.size.x;
+
+		float x = 3.0f * size / 2.0f;
+		if (PlayerID == 1) {
+			x = 1.0f - x;
+		}
+		float t = size*2.0f;
+		float y = i*((1.0f-2.0f*t) / (NUM_BUTTONS - 1)) + t;
+
+		return new Vector3(x, y, 0.0f);
+	}
+
+	Vector3 GetHealthMarkPosition(int i) {
+		var v = Camera.main.ViewportToWorldPoint(GetHealthMarkPositionViewport(i));
+		v.z = 0;
+		return v;
+	}
+
+	Vector3 GetAttackButtonPosition(int i) {
+		var v = Camera.main.ViewportToWorldPoint(GetAttackButtonPositionViewport(i));
+		v.z = 0;
+		return v;
+	}
 }
